21fa540f8b24360e820e74d3ff1dcb8f07597278
grey <shashi1842@gmail.com>
2023-02-09 05:09:17 +0530

    board and pieces drawn

---
+++ ChessEngine.py
@@ -0,0 +1,23 @@
+"""
+It will Determining valid moves at current state.
+It will keep move log.
+
+"""
+
+class GameState:
+    def __init__(self):
+        """
+        Board is an 8x8 2d list, each element in list has 2 characters.
+        The first character represents the color of the piece: 'b' or 'w'.
+        The second character represents the type of the piece: 'R', 'N', 'B', 'Q', 'K' or 'p'.
+        "--" represents an empty space with no piece.
+        """
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
